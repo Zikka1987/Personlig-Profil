02c7271bb651d80b60521d34be122c6285113068
Andreas Michaelsen <anmi0004@edu.mercantec.dk>
2024-11-24 21:58:25 +0100

    personlig

---
+++ WPF Min Personlige Profil/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace WPF_Min_Personlige_Profil
+{
+    public partial class MainWindow : Window
+    {
+
+        // Event handler for Hyperlink click
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(e.Uri.AbsoluteUri)
+            { UseShellExecute = true });
+            e.Handled = true;
+        }
+    }
+}
